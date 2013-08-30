53a900e86b1d04d865f11a1bc4204659c10d540d
drtrigon <dr.trigon@surfeu.ch>
2013-08-30 18:48:00 +0200

    bugfix for "IOError: [Errno 2] No such file or directory: '/data/project/drtrigonbot/pywikibot-compat/externals/jseg/externals/pydmtx/externals/pycolorname/PMS_cal-print.pkl'"
    Change-Id: I085ffed30e355e299b5a5b86c69b5247beabed74

--- helper.py
+++ helper.py
@@ -8,16 +8,8 @@ Created on Sat Jun 30 22:57:26 2012
 import pickle, os, sys
 
 
-scriptdir = os.path.dirname(sys.argv[0])
-if not os.path.isabs(scriptdir):
-    scriptdir = os.path.abspath(os.path.join(os.curdir, scriptdir))
-
-target = os.path.join(scriptdir, 'externals/pycolorname')
-path = list(os.path.split(os.path.abspath(os.curdir)))
-if target not in path:
-    path.append( target )
-path.append( '%s' )
-path = os.path.join(*path)
+scriptdir = os.path.dirname(os.path.abspath(__file__))
+path = os.path.join(scriptdir, '%s')
 
 
 def storeData(filename, data):
